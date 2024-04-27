2b8aa31b9dbb055e846b1b3a5e268c7dfc26720c
Neel <neel.pendharkar@gmail.com>
2024-04-27 11:37:52 +0530

    #1 Support activity log display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ActivityLogComponent } from './activity-log/activity-log.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ActivityLogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
