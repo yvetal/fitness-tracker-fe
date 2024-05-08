44b8e119f0deec396f6a0a02d63f6aba300f963b
Neel <neel.pendharkar@gmail.com>
2024-05-08 18:38:19 +0530

    Add new activity log #6

--- src/app/activity-log-add-form/activity-log-add-form.component.ts
+++ src/app/activity-log-add-form/activity-log-add-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import { ActivityLogService } from '../activity-log.service';
 
 @Component({
   selector: 'app-activity-log-add-form',
@@ -17,7 +18,24 @@ export class ActivityLogAddFormComponent {
   intensityControl = new FormControl('');
   durationHoursControl = new FormControl('');
   durationMinutesControl = new FormControl('');
+  constructor(private service: ActivityLogService) {}
   submit() {
     console.log(this.typeControl)
+    this.service.addActivityLog({
+      'type': this.typeControl.value,
+      'duration': {
+        'hours': parseInt(this.durationHoursControl.value || '0'),
+        'minutes': parseInt(this.durationMinutesControl.value || '0'),
+      },
+      'distanceInKm': parseInt(this.distanceControl.value || '0'),
+      
+      "calories": parseInt(this.caloriesControl.value || '0'),
+      "intensity": this.intensityControl
+    })
+    this.service.getActivityLogs().subscribe(
+      (response) => {
+        console.log(response)
+      }
+    )
   }
 }
